7699a15fa2cb46111ebeb37ff534a535c12f5111
Robin B <robin@broda.me>
2017-12-14 21:52:55 +0100

    Rename `reverse_icons` to `big_icon` and prioritize over `small_icon`
    (cherry picked from commit 8b6bd43f351824a674a22885e2e2bfb4f887ba81)

--- drp.py
+++ drp.py
@@ -126,14 +126,14 @@ def handle_activity(view, is_write=False):
     if state_format:
         act['state'] = state_format.format(**format_dict)
 
-    if settings.get('small_icon'):
-        act['assets']['small_image'] = get_icon(extension)
-        act['assets']['small_text'] = language
-    elif settings.get('reverse_icons'):
+    if settings.get('big_icon'):
         act['assets']['small_image'] = act['assets']['large_image']
         act['assets']['small_text'] = act['assets']['large_text']
         act['assets']['large_image'] = get_icon(extension)
         act['assets']['large_text'] = language
+    elif settings.get('small_icon'):
+        act['assets']['small_image'] = get_icon(extension)
+        act['assets']['small_text'] = language
 
     try:
         ipc.set_activity(act)
