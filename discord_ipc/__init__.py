361b96d06020617a0bf98a9ff21d606427dadfa0
Matthew Jones <stridera@gmail.com>
2019-04-23 01:30:34 -0700

    Handle removing the sublime-text path from the end of XDG_RUNTIME_DIR if both sublime and discord are snaps

--- discord_ipc/__init__.py
+++ discord_ipc/__init__.py
@@ -209,6 +209,8 @@ class UnixDiscordIpcClient(DiscordIpcClient):
         env_keys = ('XDG_RUNTIME_DIR', 'TMPDIR', 'TMP', 'TEMP')
         for env_key in env_keys:
             dir_path = os.environ.get(env_key)
+            if dir_path.endswith('snap.sublime-text'):
+                dir_path = dir_path[:-17]
             if dir_path:
                 break
         else:
