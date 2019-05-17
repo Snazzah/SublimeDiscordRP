Snazzah/SublimeDiscordRP

commit 0da997682eeaf75fd937b992322808cf63718cac

Support Sublime Text being installed as a snap (#40)
Handle removing the sublime-text path from the end of XDG_RUNTIME_DIR if both sublime and discord are snaps.

discord_ipc/__init__.py
@@ -209,6 +209,8 @@ class UnixDiscordIpcClient(DiscordIpcClient):
         env_keys = ('XDG_RUNTIME_DIR', 'TMPDIR', 'TMP', 'TEMP')
         for env_key in env_keys:
             dir_path = os.environ.get(env_key)
+            if dir_path.endswith('snap.sublime-text'):
+                dir_path = dir_path[:-17]
             if dir_path:
                 break
         else:

commit 8ccd2c9be2845a914a03f88297a72af66e39d157

Fix regression with environment variable checking
Introduced in 0da997682eeaf75fd937b992322808cf63718cac.

Fixes #41.

discord_ipc/__init__.py
@@ -209,7 +209,7 @@ class UnixDiscordIpcClient(DiscordIpcClient):
         env_keys = ('XDG_RUNTIME_DIR', 'TMPDIR', 'TMP', 'TEMP')
         for env_key in env_keys:
             dir_path = os.environ.get(env_key)
-            if dir_path.endswith('snap.sublime-text'):
+            if dir_path and dir_path.endswith('snap.sublime-text'):
                 dir_path = dir_path[:-17]
             if dir_path:
                 break